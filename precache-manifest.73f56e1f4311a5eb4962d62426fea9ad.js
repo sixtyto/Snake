74ed1ad3e9dfdd578bcf0dffd7557e2cc4c4b191
Piotr Czajkowski <sixtyto@gmail.com>
2019-12-25 22:13:13 +0100

    Updates

---
+++ precache-manifest.73f56e1f4311a5eb4962d62426fea9ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1ef6155e305c394fe3fe2b3b42efc60",
+    "url": "/snake/index.html"
+  },
+  {
+    "revision": "63b12270a0e8049d69d8",
+    "url": "/snake/static/css/main.de343c34.chunk.css"
+  },
+  {
+    "revision": "3cf8da7894a0fc81cf0a",
+    "url": "/snake/static/js/2.2cafdbcd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/snake/static/js/2.2cafdbcd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "63b12270a0e8049d69d8",
+    "url": "/snake/static/js/main.39b1bdfa.chunk.js"
+  },
+  {
+    "revision": "89de442ffe085b655632",
+    "url": "/snake/static/js/runtime-main.fa3f2afb.js"
+  }
+]);
